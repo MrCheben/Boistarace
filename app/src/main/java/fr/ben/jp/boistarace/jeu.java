9d203b761af9d8b7549aaa802040f9f35e9769ed
Quentin <yhuji.koled@gmail.com>
2018-09-17 17:45:26 +0200

    Dice + Couleur v0.5

--- app/src/main/java/fr/ben/jp/boistarace/jeu.java
+++ app/src/main/java/fr/ben/jp/boistarace/jeu.java
@@ -1,8 +1,8 @@
 package fr.ben.jp.boistarace;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class jeu extends AppCompatActivity {
@@ -29,3 +29,4 @@ public class jeu extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
