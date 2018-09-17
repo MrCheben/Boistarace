23bf90a89c0400f699f3ce664669b0bb862f4b6e
Quentin <yhuji.koled@gmail.com>
2018-09-17 17:22:30 +0200

    Dice + Couleur v0.4

--- app/src/main/java/fr/ben/jp/boistarace/jeu.java
+++ app/src/main/java/fr/ben/jp/boistarace/jeu.java
@@ -7,15 +7,25 @@ import android.view.View;
 
 public class jeu extends AppCompatActivity {
 
+    private JeuDAO maBDD = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu);
 
+        maBDD = new JeuDAO(this);
     }
-    public void rDice (View v) {
-        Intent intent=new Intent(jeu.this, dice.class);
+
+    public void rDice(View v) {
+
+        Intent intent = new Intent(jeu.this, dice.class);
+        startActivity(intent);
+    }
+
+    public void jCouleur(View v) {
+
+        Intent intent = new Intent(jeu.this, couleur.class);
         startActivity(intent);
     }
 }
